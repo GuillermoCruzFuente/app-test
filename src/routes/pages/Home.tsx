1e12ac684b84e1d70470cf596d6b8d9e84ca1c9c
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-30 00:07:13 -0600

    feat: improve home page design

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "../../styles/pages/Home.module.scss";
+
 const Home = () => {
 	return (
-		<section>
-			<h1>Home</h1>
-			<p>Welcome to my test app</p>
+		<section className={styles.homeSection}>
+			<h1>A modern Employee birthday tracking system</h1>
+
+			<p>
+				Improve the happiness of your employees and never forget that
+				special day
+			</p>
+
+			<Link to='/login' className={styles.startLink}>Start</Link>
 		</section>
 	);
 };
