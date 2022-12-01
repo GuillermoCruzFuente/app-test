56a82ba5022fb7bc14bb320a00c3364901a1a6a5
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-11-30 19:22:34 -0600

    feat: add a better description for my implementation

--- src/components/CustomNavLink.tsx
+++ src/components/CustomNavLink.tsx
@@ -1,10 +1,15 @@
 import { NavLink, NavLinkProps } from "react-router-dom";
 import styles from "../styles/components/CustomNavLink.module.scss";
-import { NavLinkActiveHandler } from "../utils/NavLinkActiveHandler";
+import type { NavLinkActiveHandler } from "../utils/NavLinkActiveHandler";
 
 const CustomNavLink = (props: NavLinkProps) => {
 	const { children, to, className, ...OtherProps } = props;
 
+	/**
+	 * There are some specificity issues with sass module styles.
+	 * Instead of match .active NavLinks (anchor tag) in scss module,
+	 * provide a programmatically way of class handling
+	 */
 	const getLinkStyles: NavLinkActiveHandler = ({ isActive }) =>
 		isActive ? ` ${styles.active} | ${className}` : `${className}`;
 
