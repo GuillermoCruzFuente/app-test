3a2235952ccd7153ad117854443395f40e52326d
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-02 06:25:11 -0600

    feat: add the drag and drop

--- src/components/DragAndDropInput.tsx
+++ src/components/DragAndDropInput.tsx
@@ -1,5 +1,30 @@
-const DragAndDropInput = () => {
-    
-}
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import styles from "../styles/components/DragAndDropInput.module.scss";
 
-export default DragAndDropInput
+const DragAndDropInput = () => {
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		console.log(acceptedFiles);
+	}, []);
+
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		onDrop,
+		accept: {
+			"image/*": [".jpg", ".jpeg", ".png", ".webp"],
+		},
+	});
+
+	return (
+		<div {...getRootProps()} className={styles.drag}>
+			<input {...getInputProps()} />
+
+			{isDragActive ? (
+				<p>Drop your file here</p>
+			) : (
+				<p>Drag your image files</p>
+			)}
+		</div>
+	);
+};
+
+export default DragAndDropInput;
