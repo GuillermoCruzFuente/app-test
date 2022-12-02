ec2a56de71e60c7aa2a1232800753f8cdf55f45e
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-02 06:30:28 -0600

    feat: wip Upload component

--- src/routes/pages/Upload.tsx
+++ src/routes/pages/Upload.tsx
@@ -1,8 +1,11 @@
+import DragAndDropInput from "../../components/DragAndDropInput";
+import styles from "../../styles/pages/Upload.module.scss";
+
 const Upload = () => {
 	return (
 		<section>
-			<h1>WIP</h1>
-			<p>This page will contain a Drag&Drop Component</p>
+			<h1 className={styles.title}>Drag & drop your image files in the box below</h1>
+			<DragAndDropInput />
 		</section>
 	);
 };
