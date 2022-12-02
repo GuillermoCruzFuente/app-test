80a008d1dfd6e2b1421834141fcd56991991d339
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-01 21:00:53 -0600

    feat: dynamic navlinks on navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,31 +1,69 @@
 import { Link } from "react-router-dom";
 import CustomNavLink from "./CustomNavLink";
 import { AppRoutes } from "../routes";
-
 import styles from "../styles/components/Navigation.module.scss";
+import useUserAuth from "../hooks/useUserAuth";
+import { useDispatch } from "react-redux";
+import { logout } from "../features/login/loginSlice";
 
 const Nav = () => {
+	const dispatch = useDispatch();
+
+	const { isUserAuthenticated } = useUserAuth();
+
+	const handleLogOut = () => {
+		dispatch(logout());
+	};
+
+	const getNavLinks = () => {
+		if (isUserAuthenticated) {
+			const authLinks = Object.values(AppRoutes).map(
+				(route) =>
+					route.path != AppRoutes.login.path && (
+						<li key={route.path}>
+							<CustomNavLink
+								to={route.path}
+								className={styles.link}
+							>
+								{route.text}
+							</CustomNavLink>
+						</li>
+					)
+			);
+
+			authLinks.push(
+				<li key={"logout"}>
+					<button className={styles.logout} onClick={handleLogOut}>
+						logOut
+					</button>
+				</li>
+			);
+
+			return authLinks;
+		} else {
+			return Object.values(AppRoutes).map(
+				(route) =>
+					!route.isProtected && (
+						<li key={route.path}>
+							<CustomNavLink
+								to={route.path}
+								className={styles.link}
+							>
+								{route.text}
+							</CustomNavLink>
+						</li>
+					)
+			);
+		}
+	};
+
 	return (
 		<nav className={styles.navbar}>
 			<Link className={styles.logo} to='/'>
 				BirthTracker
 			</Link>
 
-			<ol>
-				{Object.values(AppRoutes).map(
-					(route) =>
-						!route.protected && (
-							<li key={route.path}>
-								<CustomNavLink
-									to={route.path}
-									className={styles.link}
-								>
-									{route.text}
-								</CustomNavLink>
-							</li>
-						)
-				)}
-			</ol>
+			<ol>{getNavLinks()}</ol>
 		</nav>
 	);
 };
