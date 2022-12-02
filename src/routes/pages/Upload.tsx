91600f437d36279b9212a2d1bceebd6265807dd1
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-02 10:19:41 -0600

    feat: add Uploader to Upload page

--- src/routes/pages/Upload.tsx
+++ src/routes/pages/Upload.tsx
@@ -1,11 +1,14 @@
-import DragAndDropInput from "../../components/DragAndDropInput";
+import DragAndDropUploader from "../../components/DragAndDropUploader";
 import styles from "../../styles/pages/Upload.module.scss";
 
 const Upload = () => {
 	return (
 		<section>
-			<h1 className={styles.title}>Drag & drop your image files in the box below</h1>
-			<DragAndDropInput />
+			<h1 className={styles.title}>
+				Drag & drop your image files in the box below
+			</h1>
+			
+			<DragAndDropUploader />
 		</section>
 	);
 };
