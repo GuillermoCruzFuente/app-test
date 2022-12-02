dffcecb36bd9b98609609075db0c8aadc2a303d5
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-01 21:23:27 -0600

    feat: add Table and styles for Employees page

--- src/routes/pages/Employees.tsx
+++ src/routes/pages/Employees.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Table from "../../components/Table";
+import type { Employee, IEmployees } from "../../@types/Employee";
+import PostEmployeeForm from "../../components/PostEmployeeForm";
+import styles from "../../styles/pages/Employees.module.scss";
+import createEmployeeTableColumns from "../../components/CreateEmployeeTableColumns";
+
 const Employees = () => {
+	const { employees } = useLoaderData() as IEmployees;
+	const [data, setData] = useState<Employee[]>(employees);
+
+	// on every post fire a revalidate hook.
+	// that way the data is kept up to date
+	useEffect(() => {
+		setData(employees);
+	}, [employees]);
+
 	return (
-		<section>
-			<h1>WIP</h1>
-			<p>This page will contain a table with Employees</p>
+		<section className={styles.employeesSection}>
+			<h1>Employees</h1>
+			<p>You can check your stored data or add more</p>
+
+			<PostEmployeeForm />
+
+			<Table
+				data={data}
+				columns={createEmployeeTableColumns()}
+				pagination={10}
+			/>
 		</section>
 	);
 };
 
-export default Employees
+export default Employees;
