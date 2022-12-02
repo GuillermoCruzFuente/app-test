a8a1133f0a437d2595da4ed6ccaf45c54885d7ec
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-01 20:55:06 -0600

    feat: add Redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import router from "./routes";
+import { store } from "./store";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
