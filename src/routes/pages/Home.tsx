e5c5c2bb17e9591a7ffb84c3ef25296c184e1d54
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-01 21:33:53 -0600

    feat: add dynamic link based on UserAuth

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "../../styles/pages/Home.module.scss";
+import useUserAuth from "../../hooks/useUserAuth";
 
 const Home = () => {
+	const { isUserAuthenticated } = useUserAuth();
+
+	const defineLinkRoute = isUserAuthenticated ? "/employees" : "/login";
+
 	return (
 		<section className={styles.homeSection}>
 			<h1>A modern Employee birthday tracking system</h1>
@@ -11,7 +16,9 @@ const Home = () => {
 				special day
 			</p>
 
-			<Link to='/login' className={styles.startLink}>Start</Link>
+			<Link to={defineLinkRoute} className={styles.startLink}>
+				Start
+			</Link>
 		</section>
 	);
 };
