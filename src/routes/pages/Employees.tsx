33b7a17d8df26ddb93824314b0c23cae30d737ad
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-04 21:34:57 -0600

    feat: now, improve navigation by deferring the table data fetching

--- src/routes/pages/Employees.tsx
+++ src/routes/pages/Employees.tsx
@@ -1,33 +1,39 @@
-import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, Await } from "react-router-dom";
 import Table from "../../components/Table";
-import type { Employee, IEmployees } from "../../@types/Employee";
+import type {
+	EmployeesData,
+	EmployeesDeferredData,
+} from "../../@types/Employee";
 import PostEmployeeForm from "../../components/PostEmployeeForm";
 import styles from "../../styles/pages/Employees.module.scss";
 import createEmployeeTableColumns from "../../components/CreateEmployeeTableColumns";
+import TableLoader from "../../components/TableLoader";
+import TableError from "../../components/TableError";
 
 const Employees = () => {
-	const { employees } = useLoaderData() as IEmployees;
-	const [data, setData] = useState<Employee[]>(employees);
-
-	// on every post fire a revalidate hook.
-	// that way the data is kept up to date
-	useEffect(() => {
-		setData(employees);
-	}, [employees]);
+	const { employeesDataPromise } = useLoaderData() as EmployeesDeferredData;
 
 	return (
 		<section className={styles.employeesSection}>
 			<h1>Employees</h1>
-			<p>You can check your stored data or add more</p>
+			<p>You can check your active employees or add more to your table</p>
 
 			<PostEmployeeForm />
 
-			<Table
-				data={data}
-				columns={createEmployeeTableColumns()}
-				pagination={10}
-			/>
+			<Suspense fallback={<TableLoader />}>
+				<Await
+					resolve={employeesDataPromise}
+					errorElement={<TableError />}
+					children={(employeesData: EmployeesData) => (
+						<Table
+							data={employeesData.data.employees}
+							columns={createEmployeeTableColumns()}
+							pagination={10}
+						/>
+					)}
+				/>
+			</Suspense>
 		</section>
 	);
 };
