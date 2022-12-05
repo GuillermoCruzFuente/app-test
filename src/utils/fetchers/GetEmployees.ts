ab7954e1065587f25e4fe6cfd113235815d8ef03
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-12-04 21:38:38 -0600

    feat: change the get employees fetcher in order to return a Promise

--- src/utils/fetchers/GetEmployees.ts
+++ src/utils/fetchers/GetEmployees.ts
@@ -1,18 +1,21 @@
-import type { EmployeeResponse } from "../../@types/Employee";
+import type { EmployeesData } from "../../@types/Employee";
 import { END_POINT } from "../../data/Endpoint";
 
-const getEmployees = async (): Promise<EmployeeResponse | undefined> => {
-	try {
-		const response = await fetch(END_POINT);
+const getEmployees = async (): Promise<EmployeesData | undefined> => {
+	
+	// a chain of promises is necessary in order
+	// to provide the Employees page loader an asynchronous
+	// way to defer data
+	const getEmployeesPromise = fetch(END_POINT)
+		.then((response) => {
+			return response.json() as Promise<EmployeesData>;
+		})
+		.catch((error) => {
+			console.error(error);
+			return undefined;
+		});
 
-		const data = (await response.json()) as EmployeeResponse;
-
-		return { ...data };
-	} catch (error) {
-		console.error(error);
-
-		return undefined;
-	}
+	return getEmployeesPromise;
 };
 
 export default getEmployees;
